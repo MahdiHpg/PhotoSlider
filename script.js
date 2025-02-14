350cc19ed8fd668965fb1c8eb0f10b351a3be03e
Mahdi heydarpour <30506338+MahdiHpg@users.noreply.github.com>
2025-02-14 18:30:20 +0330

    images url fixed

--- script.js
+++ script.js
@@ -8,72 +8,72 @@ let imgIndex = 0;
 let imgs = [
   {
     id: 1,
-    img: "/imgs/1.jpg",
+    img: "imgs/1.jpg",
     alt: "1.jpg",
   },
   {
     id: 2,
-    img: "/imgs/3.jpg",
+    img: "imgs/3.jpg",
     alt: "3.jpg",
   },
   {
     id: 3,
-    img: "/imgs/bg.jpg",
+    img: "imgs/bg.jpg",
     alt: "bg.jpg",
   },
   {
     id: 4,
-    img: "/imgs/city.jpg",
+    img: "imgs/city.jpg",
     alt: "city.jpg",
   },
   {
     id: 5,
-    img: "/imgs/free.jpg",
+    img: "imgs/free.jpg",
     alt: "free.jpg",
   },
   {
     id: 6,
-    img: "/imgs/Header_Background.jpg",
+    img: "imgs/Header_Background.jpg",
     alt: "Header_Background.jpg",
   },
   {
     id: 7,
-    img: "/imgs/image1.jpg",
+    img: "imgs/image1.jpg",
     alt: "image1.jpg",
   },
   {
     id: 8,
-    img: "/imgs/image2.jpg",
+    img: "imgs/image2.jpg",
     alt: "image2.jpg",
   },
   {
     id: 9,
-    img: "/imgs/image3.jpg",
+    img: "imgs/image3.jpg",
     alt: "image3.jpg",
   },
   {
     id: 10,
-    img: "/imgs/modal-background.png",
+    img: "imgs/modal-background.png",
     alt: "modal-background.png",
   },
   {
     id: 11,
-    img: "/imgs/page-bg.jpg",
+    img: "imgs/page-bg.jpg",
     alt: "page-bg.jpg",
   },
   {
     id: 12,
-    img: "/imgs/peakpx.jpg",
+    img: "imgs/peakpx.jpg",
     alt: "peakpx.jpg",
   },
   {
     id: 13,
-    img: "/imgs/sky.jpg",
+    img: "imgs/sky.jpg",
     alt: "sky.jpg",
   },
   {
     id: 14,
-    img: "/imgs/slides-1.jpg",
+    img: "imgs/slides-1.jpg",
     alt: "slides-1.jpg",
   },
 ];
